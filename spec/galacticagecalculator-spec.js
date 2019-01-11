ef8a389bd5647863ebb18f818edc45726b1acb12
Kaveh <lcaveh@gmail.com>
2019-01-11 12:53:30 -0800

    4 new method added and tested succesfully

--- spec/galacticagecalculator-spec.js
+++ spec/galacticagecalculator-spec.js
@@ -2,12 +2,12 @@ import { GalacticAgeCalculator } from '../src/galacticagecalculator.js';
 describe('GalacticAgeCalculator', function () {
 
   let birthday;
-  let lifeStyle;
+  let lifeStyle = new Object();
   let galacticAge;
 
   beforeEach(function () {
     birthday = new Date('1979-12-12');
-    lifeStyle = new Object();
+    lifeStyle = {Gender: "Male", Bmi: 25, Smoke: "Yes", Exercise: "Yes", Blood: "Yes"}
     galacticAge = new GalacticAgeCalculator(birthday,lifeStyle);
 
   });
@@ -16,20 +16,38 @@ describe('GalacticAgeCalculator', function () {
     console.log(birthday, lifeStyle, galacticAge);
   });
 
-  it('should calculate the age in days with given birthday', function () {
+  it('it should calculate the age in days with given birthday', function () {
     galacticAge.getAge();
     expect(galacticAge.ageInDays).toEqual(14276);
   });
 
-  it('should calculate the age in days with given birthday', function () {
+  it('it should calculate the age in days with given birthday', function () {
     galacticAge.getAge();
     expect(galacticAge.ageInYears.toFixed(2)).toEqual('39.09');
   });
 
-  it('should calculate the age in other planet', function () {
+  it('it should calculate the age in other planet', function () {
     galacticAge.getAge();
     galacticAge.getAgeInPlanet("Mercury");
-    expect(galacticAge.ageInPlanet.toFixed(2)).toEqual('162.85');
+    expect(galacticAge.ageInPlanet.toFixed(2)).toEqual('162.86');
+  });
+
+  it('it should calculate average expected age', function () {
+    galacticAge.getExpectedAge();
+    expect(galacticAge.expectedAge.toFixed(2)).toEqual('73.10');
+  });
+
+  it('it should calculate the remain age to expected one', function () {
+    galacticAge.getExpectedAge();
+    galacticAge.getAge();
+    expect(galacticAge.remainAge.toFixed(2)).toEqual('34.01');
+  });
+
+  it('it should calculate the remain age to expected one in other planets', function () {
+    galacticAge.getExpectedAge();
+    galacticAge.getAge();
+    galacticAge.getRemainAgeInPlanet("Venus");
+    expect(galacticAge.remainAgeInPlanet.toFixed(2)).toEqual('54.86');
   });
 
 });
--- src/galacticagecalculator.js
+++ src/galacticagecalculator.js
@@ -5,9 +5,39 @@ export class GalacticAgeCalculator {
     this.lifeStyle = lifeStyle;
     this.ageInDays = 0;
     this.ageInYears = 0.00;
-    this.planetYears = {Mercury:0.24, Venus:0.62, Earth:1, Mars:1.88, Jupiter:11.86}
+    this.planetYears = {Mercury:0.24, Venus:0.62, Earth:1, Mars:1.88, Jupiter:11.86};
     this.ageInPlanet = 0.00;
+    this.expectedAge = 0;
+    this.remainAge = 0;
+    this.remainAgeInPlanet= 0;
 
+  }
+
+  getExpectedAge(){
+    this.expectedAge = 76.1;
+    if (this.lifeStyle["Gender"]=="Female"){
+      this.expectedAge+= 5;
+    }
+    if (this.lifeStyle["Bmi"]>30){
+      this.expectedAge-= 10;
+    } else if (this.lifeStyle["Bmi"]>25){
+      this.expectedAge-= 3;
+    } else if (this.lifeStyle["Bmi"]>20){
+      this.expectedAge+= 5;
+    } else if (this.lifeStyle["Bmi"]>15){
+      this.expectedAge-= 3;
+    } else {
+      this.expectedAge-= 10;
+    }
+    if (this.lifeStyle["Smoke"]=="Yes"){
+      this.expectedAge-= 8;
+    }
+    if (this.lifeStyle["Exercise"]=="Yes"){
+      this.expectedAge+= 10;
+    }
+    if (this.lifeStyle["Blood"]=="Yes"){
+      this.expectedAge-= 10;
+    }
   }
 
   getAge() {
@@ -16,8 +46,12 @@ export class GalacticAgeCalculator {
     let difference = this.today-this.birthday;
     this.ageInDays= Math.round(difference/milisecondsInDay);
     this.ageInYears=difference/milisecondsInYear;
+    this.remainAge= this.expectedAge-this.ageInYears;
   }
   getAgeInPlanet(planet) {
     this.ageInPlanet= this.ageInYears/this.planetYears[planet];
+  }
+  getRemainAgeInPlanet(planet) {
+    this.remainAgeInPlanet= this.remainAge/this.planetYears[planet];
   }
 }
