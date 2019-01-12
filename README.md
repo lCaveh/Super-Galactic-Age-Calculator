8e3b7d9675c5599204e300f86c75993bf09eb707
Kaveh <lcaveh@gmail.com>
2019-01-11 17:00:54 -0800

    Front end finished

---
+++ README.md
@@ -0,0 +1,36 @@
+# Galactic Age Calculator
+
+#### By Kaveh Saleminejad
+
+## Description
+
+this program will get the birthday and some life style informations from the user. Based on those data it will calculate the current age and expected age of the user. Then the user can click on different planets picture to see his/her age and remaining age there.
+
+## Specifications
+
+
+
+
+## Setup/Installation Requirements
+
+  - Clone this repository from https://github.com/lCaveh/Super-Galactic-Age-Calculator.git
+  - Navigate to the "Super-Galactic-Age-Calculator" folder in your terminal. Type "npm i" to install necessary plugins.
+  - Type in "npm run start" to start the app in your Chrome browswer.
+
+
+## Support and contact details
+
+  - Kaveh Saleminejad - lcaveh@gmail.com
+
+## Technologies Used
+
+  - JavaScript
+  - Node.js
+  - Git
+  - GitHub
+  - CSS
+  - HTML
+
+This software is licensed under the MIT license.
+
+Copyright (c) 2018 **Kaveh Saleminejad**
--- src/main.js
+++ src/main.js
@@ -10,10 +10,35 @@ $(document).ready(function () {
   $("img#form").attr("src",formImage);
   $('#calculatorForm').submit(function (event) {
     event.preventDefault();
+    let planets=["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"];
+    let birthday = $('input#birthday').val();
+    let gender = $('#gender').val();
+    let bmi = $('input#bmi').val();
+    let smoke = $('#smoke').val();
+    let exercise = $('#exercise').val();
+    let blood = $('#blood').val();
+    let lifestyle = {Gender:gender,Bmi:bmi,Smoke:smoke,Exercise:exercise,Blood:blood};
+    let galacticAge= new GalacticAgeCalculator(birthday,lifestyle);
     $(".form").hide();
-
     $(".result").show();
-    $("img#mercury").attr("src",formImage);
+    galacticAge.getExpectedAge();
+    galacticAge.getAge();
+    for (var planet in planets){
+      $(`img#${planets[planet]}`).attr("src",galacticAge.planetImages[`${planets[planet]}`]);
+      $(`img#${planets[planet]}`).click(function(){
+        $("img#Show").attr("src",galacticAge.planetImages[this.id]);
+        galacticAge.getAgeInPlanet(this.id);
+        galacticAge.getRemainAgeInPlanet(this.id);
+        $("p.age").text(`Your age at ${this.id} planet is ${galacticAge.ageInPlanet.toFixed(2)}`);
+        if (galacticAge.remainAge>=0){
+          $("p.remainAge").text(`You supposed to live ${galacticAge.remainAgeInPlanet.toFixed(2)} years more in ${this.id}`);
+        } else {
+          galacticAge.remainAge= Math.abs(galacticAge.remainAge);
+          $("p.remainAge").text(`You lived past years from the life expectancy ${galacticAge.remainAgeInPlanet.toFixed(2)}`);
+
+        }
+      });
+    }
 
 
 
